5bf7c5096bc1d308a15b235e58189cec81cb97f2
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2023-10-31 16:05:21 +0100

    Fixed bug in interpreting scores in js

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,10 @@
-function printMessage(msg){
+function printMessage(msg) {
     let div = document.createElement('div');
     div.innerHTML = msg;
     document.getElementById('messages').appendChild(div);
 }
 
-function printScore(score, elementID){
+function printScore(score, elementID) {
     let div = document.createElement('div');
     div.innerHTML = score;
     document.getElementById(elementID).appendChild(div);
@@ -18,10 +18,22 @@ function uncheck(checkboxID) {
     document.getElementById(myCheck).checked = false;
 }
 
-function clearMessages(){
+function clearMessages() {
     document.getElementById('messages').innerHTML = '';
 }
 
-function clearScore(elementID){
+function clearScore(elementID) {
     document.getElementById(elementID).innerHTML = '';
+}
+
+function disableBtns() {
+    document.getElementById('play-rock').disabled = true;
+    document.getElementById('play-paper').disabled = true;
+    document.getElementById('play-scissors').disabled = true;
+}
+
+function enableBtns() {
+    document.getElementById('play-rock').disabled = false;
+    document.getElementById('play-paper').disabled = false;
+    document.getElementById('play-scissors').disabled = false;
 }
--- js/script.js
+++ js/script.js
@@ -13,53 +13,34 @@ function playGame(playerInput) {
         } else if (argMoveId == 3) {
             return 'scissors';
         }
-
-        printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-        return 'nieznany ruch';
     }
 
-    // function displayResult(argComputerMove, argPlayerMove) {
-    //     if (argComputerMove == 'rock' && argPlayerMove == 'paper') {
-    //         printMessage('Wygrałeś!');
-    //     } else if (argComputerMove == 'paper' && argPlayerMove == 'scissors') {
-    //         printMessage('Wygrałeś!');
-    //     } else if (argComputerMove == 'scissors' && argPlayerMove == 'rock') {
-    //         printMessage('Wygrałeś!');
-    //     } else if (argComputerMove == argPlayerMove) {
-    //         printMessage('Remis!');
-    //     } else if (argPlayerMove == 'nieznany ruch' || argComputerMove == 'nieznany ruch') {
-    //         printMessage('Niestety ktoś z nas wykonał nieznany ruch :/');
-    //     } else {
-    //         printMessage('Przegrałeś :<');
-    //     }
-    // }
-    function countScore(computerNum, playerNum) {
-        if (computerNum > playerNum) {
-            whichRivalFlag = 0;
-            computerScore = computerScore + 1;
-            console.log('Wynik komputera: ' + computerScore);
-            return computerScore;
-        } else if (computerNum < playerNum) {
+    function countScore(ComputerMove, PlayerMove) {
+        if (ComputerMove == PlayerMove) {
+            return 0;
+        } else if (ComputerMove == 'rock' && PlayerMove == 'paper') {
             whichRivalFlag = 1;
             playerScore = playerScore + 1;
-            console.log('Wynik gracza: ' + playerScore);
+            return playerScore;
+        } else if (ComputerMove == 'paper' && PlayerMove == 'scissors') {
+            whichRivalFlag = 1;
+            playerScore = playerScore + 1;
+            return playerScore;
+        } else if (ComputerMove == 'scissors' && PlayerMove == 'rock') {
+            whichRivalFlag = 1;
+            playerScore = playerScore + 1;
             return playerScore;
         } else {
-            return 0;
+            whichRivalFlag = 0;
+            computerScore = computerScore + 1;
+            return computerScore;
         }
     }
 
     let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-    console.log('Wylosowana liczba to: ' + randomNumber);
-
     let computerMove = getMoveName(randomNumber);
-
-    printMessage('Mój ruch:');
-
-    console.log('Gracz wpisał: ' + playerInput);
-
-    let score = countScore(randomNumber, playerInput);
+    let playerMove = getMoveName(playerInput);
+    let score = countScore(computerMove, playerMove);
 
     console.log('Wynik: ' + score);
     console.log('Flaga: ' + whichRivalFlag);
@@ -70,22 +51,17 @@ function playGame(playerInput) {
         elementID = 'playerScore';
     }
 
-    console.log('Element ID: ' + elementID);
-
-    if (score !== 0){
+    if (score !== 0) {
         clearScore(elementID);
         printScore(score, elementID);
     }
-
-    let playerMove = getMoveName(playerInput);
 
     let checkboxID = computerMove + '-' + playerMove;
 
     check(checkboxID);
 
+    printMessage('Mój ruch:');
     printMessage('Twój ruch:');
-
-    // displayResult(computerMove, playerMove);
 }
 
 printScore(0, 'computerScore');
@@ -93,13 +69,17 @@ printScore(0, 'playerScore');
 
 document.getElementById('play-rock').addEventListener('click', function () {
     playGame('1');
+    disableBtns();
 });
 document.getElementById('play-paper').addEventListener('click', function () {
     playGame('2');
+    disableBtns();
 });
 document.getElementById('play-scissors').addEventListener('click', function () {
     playGame('3');
+    disableBtns();
 });
 document.getElementById('resetInput').addEventListener('click', function () {
     clearMessages();
+    enableBtns();
 });
